c67163059660add5a8eaedc2648896e57210497d
sandeep <sandeepkrpatel2002@gmail.com>
2023-05-23 02:32:46 +0530

    sandeep

---
+++ index.py
@@ -0,0 +1,34 @@
+# Python program to convert
+# text file to pdf file
+ 
+ 
+from fpdf import FPDF
+  
+# save FPDF() class into
+# a variable pdf
+pdf = FPDF()  
+  
+# Add a page
+pdf.add_page()
+  
+# set style and size of font
+# that you want in the pdf
+pdf.set_font("Arial", size = 15)
+
+ 
+# create a cell
+pdf.cell(200, 10, txt = "Sandeep Patel",border=1,
+         ln = 3, align = 'C')
+ 
+# add another cell
+pdf.cell(200, 40, txt = "This is for practise",
+         ln = 2, align = 'C')
+# open the text file in read mode
+f = open("myfile.txt", "r")
+ 
+# insert the texts in pdf
+for x in f:
+    pdf.cell(200, 10, txt = x, ln = 1, align = 'C')
+  
+# save the pdf with name .pdf
+pdf.output("sandeep.pdf")
